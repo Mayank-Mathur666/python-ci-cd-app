2132773e8007883446ce7268e055eec76188b52f
Ubuntu <ubuntu@ip-172-31-14-231.ap-south-1.compute.internal>
2026-01-06 10:56:04 +0000

    Initial Python CI/CD app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Version 2 deployed successfully"
+    return "index.html"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
